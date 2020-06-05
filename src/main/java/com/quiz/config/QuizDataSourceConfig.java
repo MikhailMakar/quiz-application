80f14dd7eb75152f4c52ee723479b03e343de49b
Mikhail_Makar <mikhail.makar@clarivate.com>
2020-06-05 11:42:20 +0300

    fixed configuration

--- src/main/java/com/quiz/config/QuizDataSourceConfig.java
+++ src/main/java/com/quiz/config/QuizDataSourceConfig.java
@@ -1,60 +1,9 @@
 package com.quiz.config;
 
-import com.zaxxer.hikari.HikariDataSource;
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateProperties;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateSettings;
-import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-@EnableJpaRepositories(basePackages = {"com.quiz.repository"},
-        entityManagerFactoryRef = "quizEntityManagerFactory",
-        transactionManagerRef = "quizTransactionManager")
+@EnableJpaRepositories(basePackages = {"com.quiz.repository"})
 public class QuizDataSourceConfig {
-
-    @Bean(name = "quizDataSourceProperties")
-    @Primary
-    @ConfigurationProperties("app.datasource.quiz")
-    public DataSourceProperties quizDataSourceProperties() {
-        return new DataSourceProperties();
-    }
-
-    @Bean(name = "quizDataSource")
-    @Primary
-    public DataSource quizDataSource(@Qualifier("quizDataSourceProperties") DataSourceProperties dataSourceProperties) {
-        return dataSourceProperties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
-    }
-
-    @Bean(name = "quizEntityManagerFactory")
-    @Primary
-    public LocalContainerEntityManagerFactoryBean quizEntityManagerFactory(EntityManagerFactoryBuilder builder,
-                                                                              @Qualifier("quizDataSource") DataSource dataSource,
-                                                                              HibernateProperties hibernateProperties,
-                                                                              JpaProperties jpaProperties) {
-        HibernateSettings hibernateSettings = new HibernateSettings();
-        return builder
-                .dataSource(dataSource)
-                .packages("com.quiz.model")
-                .persistenceUnit("quizEntityManager")
-                .properties(hibernateProperties.determineHibernateProperties(jpaProperties.getProperties(), hibernateSettings))
-                .build();
-    }
-
-    @Bean
-    @Primary
-    public PlatformTransactionManager quizTransactionManager(@Qualifier("quizEntityManagerFactory") EntityManagerFactory quizEntityManagerFactory) {
-        return new JpaTransactionManager(quizEntityManagerFactory);
-    }
 }
