9b9a9a541f8d7ac4126410582f4f339e9fe2004e
Mikhail_Makar <mikhail.makar@clarivate.com>
2020-06-09 19:17:07 +0300

    tests

---
+++ src/test/java/com/quiz/controller/AnswerControllerTest.java
@@ -0,0 +1,24 @@
+package com.quiz.controller;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AnswerControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getAllAnswersTest() throws Exception {
+        this.mockMvc.perform(get("/answers/all"))
+                .andExpect(status().isOk());
+    }
+}
