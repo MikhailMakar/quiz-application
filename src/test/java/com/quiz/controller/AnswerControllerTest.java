dabf16afe4e8cd8ee030a19744abee338ac69a2d
Zagorodskii, Maksim <Maksim.Zagorodskii@Clarivate.com>
2020-06-11 17:52:25 +0300

    fix tests

--- src/test/java/com/quiz/controller/AnswerControllerTest.java
+++ src/test/java/com/quiz/controller/AnswerControllerTest.java
@@ -1,20 +1,28 @@
 package com.quiz.controller;
 
+import com.quiz.model.Answer;
+import com.quiz.service.AnswerService;
+import com.quiz.service.QuestionService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = AnswerController.class)
 public class AnswerControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private AnswerService answerService;
 
     @Test
     public void getAllAnswersTest() throws Exception {
